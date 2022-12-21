0085ece15298d81a348250f3b2b259af1930708b
Tung Nguyen <ohmen2408@gmail.com>
2022-12-21 09:31:56 +0700

    setup axios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+import axiosClient from './api/axiosClient'
+import postApi from './api/postApi'
+
 console.log('hello world')
+
+function main() {
+  //   const response = axiosClient('/posts')
+  //   console.log(response)
+  const querryParams = {
+    _page: 1,
+    _limit: 5,
+  }
+
+  const response = postApi.getAll(querryParams)
+  console.log(postApi)
+}
+
+main()
