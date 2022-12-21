18ed8e821358c2639b6c8fbbbe6694287752afad
Tung Nguyen <ohmen2408@gmail.com>
2022-12-21 14:54:47 +0700

    interceptor

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log('hello world')
+<<<<<<< Updated upstream
+=======
+
+function main() {
+  //   const response = axiosClient('/posts')
+  //   console.log(response)
+  try {
+    const querryParams = {
+      _page: 1,
+      _limit: 5,
+    }
+
+    const response = postApi.getAll(querryParams)
+    console.log(postApi)
+  } catch (error) {}
+}
+
+main()
+>>>>>>> Stashed changes
