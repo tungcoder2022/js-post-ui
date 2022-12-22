7d75e9655b8462f6f0c72560db1e62fb09129b40
Tung Nguyen <ohmen2408@gmail.com>
2022-12-22 15:14:11 +0700

    Pagination
    - Bind click event
    - Disable page links
    - Refetch data on page change

--- js/home.js
+++ js/home.js
@@ -1,6 +1,5 @@
-import axiosClient from './api/axiosClient'
 import postApi from './api/postApi'
-import { setTextContent, truncateText } from './utils'
+import { getUlPagination, setTextContent, truncateText } from './utils'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
@@ -44,22 +43,115 @@ function renderPostList(postList) {
   const ulElement = document.getElementById('postList')
   if (!ulElement) return
 
+  // clear current list
+  ulElement.textContent = ''
+
   postList.forEach((post) => {
     const liElement = createPostElement(post)
     ulElement.appendChild(liElement)
   })
 }
 
+function renderPagination(pagination) {
+  const ulPagination = getUlPagination()
+  if (!pagination || !ulPagination) return
+
+  // calc totalPages
+  const { _page, _limit, _totalRows } = pagination
+  const totalPages = Math.ceil(_totalRows / _limit)
+  // save page and totalPages to ulPagination
+  ulPagination.dataset.page = _page
+  ulPagination.dataset.totalPages = totalPages
+
+  // check if enable/disable prev/next link
+  if (_page <= 1) ulPagination.firstElementChild?.classList.add('disabled')
+  else ulPagination.firstElementChild?.classList.remove('disabled')
+
+  if (_page >= totalPages) ulPagination.lastElementChild?.classList.add('disabled')
+  else ulPagination.lastElementChild?.classList.remove('disabled')
+}
+
+async function handleFilterChange(filterName, filterValue) {
+  try {
+    // update query params
+    const url = new URL(window.location)
+    url.searchParams.set(filterName, filterValue)
+    history.pushState({}, '', url)
+
+    const { data, pagination } = await postApi.getAll(url.searchParams)
+    renderPostList(data)
+    renderPagination(pagination)
+  } catch (error) {
+    console.log('failed to fetch post list', error)
+  }
+}
+
+function handlePrevClick(e) {
+  e.preventDefault()
+
+  const ulPagination = getUlPagination()
+  if (!ulPagination) return
+
+  const page = Number.parseInt(ulPagination.dataset.page) || 1
+  if (page <= 1) return
+
+  handleFilterChange('_page', page - 1)
+}
+function handleNextClick(e) {
+  e.preventDefault()
+
+  const ulPagination = getUlPagination()
+  if (!ulPagination) return
+
+  const page = Number.parseInt(ulPagination.dataset.page) || 1
+  const totalPages = ulPagination.dataset.totalPages
+  if (page >= totalPages) return
+
+  handleFilterChange('_page', page + 1)
+}
+
+function initPagination() {
+  // bind click event for prev/next link
+  const ulPagination = getUlPagination()
+  if (!ulPagination) return
+
+  // add click event for prev link
+  const prevLink = ulPagination.firstElementChild?.firstElementChild
+  if (prevLink) {
+    prevLink.addEventListener('click', handlePrevClick)
+  }
+
+  // add click event for next link
+  const nextLink = ulPagination.lastElementChild?.lastElementChild
+  if (nextLink) {
+    nextLink.addEventListener('click', handleNextClick)
+  }
+}
+
+function initUrl() {
+  const url = new URL(window.location)
+
+  // update search params if needed
+  if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1)
+  if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6)
+
+  history.pushState({}, '', url)
+}
+
 // main
 ;(async () => {
   try {
-    const querryParams = {
-      _page: 1,
-      _limit: 6,
-    }
+    // attach click event for links
+    initPagination()
 
+    // set default pagination(_page, _limit) on Url
+    initUrl()
+
+    // render post list basee URL params
+    const querryParams = new URLSearchParams(window.location.search)
     const { data, pagination } = await postApi.getAll(querryParams)
     renderPostList(data)
+    renderPagination(pagination)
   } catch (error) {
     console.log('get all failed', error)
     // show modal, toast error
